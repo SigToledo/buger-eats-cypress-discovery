f3959d76c665150f2a34b5379fa4b9ede407e0bf
Luiz Toledo <Luiz Toledo>
2022-06-23 08:46:12 -0300

    meu primeiro commit no git

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl:'https://buger-eats-qa.vercel.app' 
+  },
+  viewportWidth: 1440,
+  viewportHeight: 900, /*1440,900 */
+});
